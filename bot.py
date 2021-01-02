1fc97be4b943ba4b89323e10aab44b03963d2db4
Andrew McKnight <thedonk@gmail.com>
2021-01-02 02:26:47 -0800

    Initial commit (echo bot)

---
+++ bot.py
@@ -0,0 +1,37 @@
+# bot.py
+import os # for importing env vars for the bot to use
+from twitchio.ext import commands
+
+bot = commands.Bot(
+    # set up the bot
+    irc_token=os.environ['TMI_TOKEN'],
+    client_id=os.environ['CLIENT_ID'],
+    nick=os.environ['BOT_NICK'],
+    prefix=os.environ['BOT_PREFIX'],
+    initial_channels=[os.environ['CHANNEL']]
+)
+
+# bot.py, below bot object
+@bot.event
+async def event_ready():
+    'Called once when the bot goes online.'
+    print(f"{os.environ['BOT_NICK']} is online!")
+    ws = bot._ws  # this is only needed to send messages within event_ready
+    await ws.send_privmsg(os.environ['CHANNEL'], f"/me has landed!")
+
+# bot.py, below event_ready
+@bot.event
+async def event_message(ctx):
+    'Runs every time a message is sent in chat.'
+
+    # make sure the bot ignores itself and the streamer
+    if ctx.author.name.lower() == os.environ['BOT_NICK'].lower():
+        return
+
+    # bot.py, in event_message, below the bot-ignoring stuff
+    await ctx.channel.send(ctx.content)
+
+# bot.py
+if __name__ == "__main__":
+    bot.run()
+
