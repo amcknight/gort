5cc36a628823ca5fde9f457f9530340b102e6f8a
Andrew McKnight <thedonk@gmail.com>
2023-01-23 18:13:57 -0800

    Updating twitchio to use wildcard instead of a git address

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ class Bot(commands.Bot):
         
         super().__init__(
             token=os.environ['TMI_TOKEN'],
-            client_id=os.environ['CLIENT_ID'],
+            client_secret=os.environ['CLIENT_SECRET'],
             nick=name,
             prefix='!',
             initial_channels=[os.environ['CHANNEL']]
