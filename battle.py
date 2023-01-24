2bd651d222e51a2066b275bde8d76fee4a84e765
Andrew McKnight <thedonk@gmail.com>
2023-01-24 15:37:29 -0800

    Added beginning of battle simulator

---
+++ battle.py
@@ -0,0 +1,12 @@
+from random import randrange
+
+class Battle:
+    def __init__(self):
+        pass
+
+    def run(self):
+        print("You win!")
+
+
+if __name__ == "__main__":
+    Battle().run()
